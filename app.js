9b19d216dbc8b6250d120ba81d88408d71c3f345
muthylasreedharroyal <144820808+muthylasreedharroyal@users.noreply.github.com>
2024-04-14 09:15:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { ReactDOM } from "react-dom/client";
+const Card=(props)=>{<div className="box">
+<img className="food-img"src="https://images.pexels.com/photos/1860208/pexels-photo-1860208.jpeg?cs=srgb&dl=cooked-food-1860208.jpg&fm=jpg"></img>
+
+
+
+
+</div>
+
+
+
+
+
+
+}
